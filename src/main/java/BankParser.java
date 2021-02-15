ff144db57b6f6f329fe5c750a27393825a1e23e1
Hoidi <tobiakar@student.chalmers.se>
2021-02-15 21:23:58 +0100

    Add ability to parse empty and new style of bank transactions

--- src/main/java/BankParser.java
+++ src/main/java/BankParser.java
@@ -4,7 +4,7 @@ import java.util.Collections;
 
 public class BankParser {
 
-    public static ArrayList<ArrayList<String>> getLinesBank(String text, String fromDate) {
+    public static ArrayList<ArrayList<String>> getLinesBankOld(String text, String fromDate) {
         ArrayList<String> tmpLines = new ArrayList<>(Arrays.asList(text.split("\\r?\\n")));
         ArrayList<ArrayList<String>> lines = new ArrayList<>();
 
@@ -27,13 +27,108 @@ public class BankParser {
         lines.remove(lines.size()-1); // another hidden "(3)"
 
         // remove all things that happened before the book keeping date
+        removeBeforeDate(lines,fromDate);
+        return lines;
+    }
+
+    public static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
+    }
+
+    public static ItemHolder<Item> createBankBank(String text, String fromDate) {
+        ArrayList<ArrayList<String>> lines = null;
+        if (text.startsWith("Skapad")) {
+            lines = BankParser.getLinesBankOld(text,fromDate);
+        } else {
+            lines = BankParser.getLinesBankNew(text,fromDate);
+        }
+
+
+        double saldoIn = Double.parseDouble(String.join("",lines.remove(0)).replace(",","."));
+
+        lines.get(lines.size()-1).remove(0);
+        lines.get(lines.size()-1).remove(0);
+        double saldoUt = Double.parseDouble(String.join("",lines.remove(lines.size()-1)).replace(",","."));
+
+        ItemHolder<Item> bankBank = new ItemHolder<Item>(saldoIn,saldoUt,fromDate);
+
+        Rev.MyDouble lastSaldo = new Rev.MyDouble(saldoIn);
+
+        for (ArrayList<String> s : lines) {
+            Item b = createBankItem(s,lastSaldo);
+            bankBank.addItem(b);
+        }
+
+        return bankBank;
+    }
+
+    private static ArrayList<ArrayList<String>> getLinesBankNew(String text, String fromDate) {
+        ArrayList<String> tmpLines = new ArrayList<>(Arrays.asList(text.split("\\r?\\n")));
+        ArrayList<ArrayList<String>> lines = new ArrayList<>();
+
+        for (int i = 0; i < tmpLines.size(); i++) {
+            lines.add(new ArrayList<>(Arrays.asList(tmpLines.get(i).split("\\s"))));
+        }
+
+        // TODO: Fix this when an example comes up, for now just fix the first page
+        // removes the first few lines on each page
+        /*for (int i = 0; i < lines.size(); i++) {
+            if (lines.get(i).get(0).equals("Bokförings")) {
+                if (i>0) {
+                    lines.remove(i-- -1); // there are some hidden "(3)"
+                }
+                for (int x = 0; x < 8; x++) {
+                    lines.remove(i);
+                }
+            }
+        }*/
+        for (int i = 0; i < 8; i++) {
+            lines.remove(0);
+        }
+
+        for (int i = 0; i < 8; i++) {
+            lines.remove(lines.size()-1);
+        }
+
+
+        Collections.reverse(lines);
+
+        lines.get(0).remove(0);
+
+        // convert bookkeeping date from YYYY-MM-DD to YYMMDD
+        convertDates(lines);
+
+        // remove all things that happened before the book keeping date
+        removeBeforeDate(lines,fromDate);
+
+        return lines;
+    }
+
+    /**
+     * Removes all the stuff that happened before the fromDate. Also sets the first line to only the amount of money going in
+     *
+     * @param lines
+     * @param fromDate
+     */
+    private static void removeBeforeDate(ArrayList<ArrayList<String>> lines, String fromDate) {
         for (int i = 1; i < lines.size(); i++) {
-            if (Integer.parseInt(fromDate) > Integer.parseInt(lines.get(1).get(0))) {
+            if (lines.size() == 2) { // nothing has happened
+                lines.get(0).subList(0, 3).clear();
+                System.out.println("");
+                String inStr = String.join("", lines.get(0));
+                lines.get(0).clear();
+                lines.get(0).add(inStr);
+            }
+            else if (Integer.parseInt(fromDate) > Integer.parseInt(lines.get(1).get(0))) {
                 lines.remove(1);
             } else { // put the "money in" as the only element in the first row
                 lines.remove(0);
                 ArrayList<String> firstLine = new ArrayList<>(lines.get(0));
-                lines.get(0).remove(0);
                 lines.get(0).remove(0);
                 lines.get(0).remove(0);
                 String oldStr = String.join(" ", lines.get(0));
@@ -60,37 +155,14 @@ public class BankParser {
                 break;
             }
         }
-        return lines;
     }
 
-    public static boolean isNumeric(String str) {
-        try {
-            Double.parseDouble(str);
-            return true;
-        } catch(NumberFormatException e){
-            return false;
+    private static void convertDates(ArrayList<ArrayList<String>> lines) {
+        for (int i = 1; i < lines.size()-1; i++) {
+            String bigDate = lines.get(i).get(0);
+            String smallDate = bigDate.replace("-","").substring(2);
+            lines.get(i).set(0,smallDate);
         }
-    }
-
-    public static ItemHolder<Item> createBankBank(String text, String fromDate) {
-        ArrayList<ArrayList<String>> lines = BankParser.getLinesBank(text,fromDate);
-
-        double saldoIn = Double.parseDouble(String.join("",lines.remove(0)).replace(",","."));
-
-        lines.get(lines.size()-1).remove(0);
-        lines.get(lines.size()-1).remove(0);
-        double saldoUt = Double.parseDouble(String.join("",lines.remove(lines.size()-1)).replace(",","."));
-
-        ItemHolder<Item> bankBank = new ItemHolder<Item>(saldoIn,saldoUt,fromDate);
-
-        Rev.MyDouble lastSaldo = new Rev.MyDouble(saldoIn);
-
-        for (ArrayList<String> s : lines) {
-            Item b = createBankItem(s,lastSaldo);
-            bankBank.addItem(b);
-        }
-
-        return bankBank;
     }
 
     public static Item createBankItem(ArrayList<String> s, Rev.MyDouble lastSaldoDouble) {
